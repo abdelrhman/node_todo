a5cec0255e19d0293525cbe63c51e2b25570cf0a
abdelrhman <abdelrhmantalat.93@gmail.com>
2018-05-27 15:12:23 +0200

    user login

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 require('./config/config');
 const { mongoose } = require('./db/mongoose');
@@ -135,6 +136,21 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password)
+    .then(user => {
+      return user.genrateAuthToken().then(token => {
+        res.header('x-auth', token).send({ user });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).send();
+    });
 });
 
 app.listen(PORT, () => {
